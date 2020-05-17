861cbe4a07b6051569220ead8ce9def6720e2675
Yongzhi Liu <yongzhiliu@Yongzhis-MacBook-Pro.local>
2020-05-17 11:37:44 +0800

    should_return_sellIn_8_quality_47_when_backstagePass_pass_1_day_given_sellIn_9_quality_45

--- src/test/java/cn/xpbootcamp/gildedrose/BackstagePassTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/BackstagePassTest.java
@@ -20,4 +20,12 @@ class BackstagePassTest {
         assertThat(backstagePass.getSellIn()).isEqualTo(9);
         assertThat(backstagePass.getQuality()).isEqualTo(47);
     }
+
+    @Test
+    public void should_return_sellIn_8_quality_47_when_backstagePass_pass_1_day_given_sellIn_9_quality_45(){
+        BackstagePass backstagePass = new BackstagePass(9,45);
+        backstagePass.oneDayPassed();
+        assertThat(backstagePass.getSellIn()).isEqualTo(8);
+        assertThat(backstagePass.getQuality()).isEqualTo(47);
+    }
 }
