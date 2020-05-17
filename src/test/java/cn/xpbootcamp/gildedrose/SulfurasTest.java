5660b25cdb85192d30d301f4a4d04be664ef4740
Yongzhi Liu <yongzhiliu@Yongzhis-MacBook-Pro.local>
2020-05-17 11:45:45 +0800

    should_return_sellIn_n2_quality_1_when_sulfuras_pass_1_day_given_sellIn_n2_quality_1

--- src/test/java/cn/xpbootcamp/gildedrose/SulfurasTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/SulfurasTest.java
@@ -36,4 +36,12 @@ class SulfurasTest {
         assertThat(sulfuras.getSellIn()).isEqualTo(-1);
         assertThat(sulfuras.getQuality()).isEqualTo(1);
     }
+
+    @Test
+    public void should_return_sellIn_n2_quality_1_when_sulfuras_pass_1_day_given_sellIn_n2_quality_1 () {
+        Sulfuras sulfuras = new Sulfuras(-2,1);
+        sulfuras.oneDayPassed();
+        assertThat(sulfuras.getSellIn()).isEqualTo(-2);
+        assertThat(sulfuras.getQuality()).isEqualTo(1);
+    }
 }
