f9a2a3fa82d561145b98a1b40fda17cc90043438
Yongzhi Liu <yongzhiliu@Yongzhis-MacBook-Pro.local>
2020-05-17 11:48:55 +0800

    should_return_sellIn_1_quality_50_when_agedBrie_pass_1_day_given_sellIn_2_quality_49

--- src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
@@ -13,4 +13,11 @@ class AgedBrieTest {
         assertThat(agedBrie.getQuality()).isEqualTo(1);
     }
 
+    @Test
+    public void should_return_sellIn_1_quality_50_when_agedBrie_pass_1_day_given_sellIn_2_quality_49 () {
+        AgedBrie agedBrie = new AgedBrie(2,49);
+        agedBrie.oneDayPassed();
+        assertThat(agedBrie.getSellIn()).isEqualTo(1);
+        assertThat(agedBrie.getQuality()).isEqualTo(50);
+    }
 }
